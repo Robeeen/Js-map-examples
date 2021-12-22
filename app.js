5b0ae08faa740300dcdad4f831f64837a6762c78
Shams Arefin Khan <robeeen@gmail.com>
2021-12-23 00:29:47 +0600

    Implement Unique Values

--- app.js
+++ app.js
@@ -20,15 +20,21 @@ const people = [
       age: 26,
       position: 'intern',
     },
+    {
+      name: 'susy',
+      age: 30,
+      position: 'messenger'
+    }
   ];
 
 //a good example of .map()
 // returns a new array
 // does not change the size of original array (unlike filter)
 // uses values from original array when making new one
+// will integrate the Unique Values.
 
-//grab all the ages from array:
-const ages = people.map((person) => person.age);
+//grab all the ages from array: & Unique values
+const ages = ['all', ...new Set(people.map((person) => person.age))];
 console.log(ages);
 
 const getDiv = document.querySelector('#ages');
@@ -46,10 +52,12 @@ const newAge = people.map((person) => {
 console.log(newAge);
 
 //grab the name to show in Div on index.html (innerHTML)
-const names = people.map((item) => `<h3>Name: ${item.name}</h3>`);
+const names = ['all', ...new Set(people.map((item) => `${item.name}`))];
 
 //get the id of the div.
 const result =  document.querySelector('#result');
 
-//Assign the data into Div ID
-result.innerHTML = names.join('');
+//Assign the data into Div ID & get the unique values only.
+result.innerHTML = names.map((names) => {
+    return `<button> ${names} </button>`
+}).join(''); // join to remove the (,)
